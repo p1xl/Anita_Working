55529358659685315203c5b3f0263fa85386c9c6
p1xl <rojaun_reid@yahoo.com>
2017-08-13 11:35:43 -0500

    fixed update cart issue

--- admin/parsers/update_cart.php
+++ admin/parsers/update_cart.php
@@ -1,30 +1,37 @@
 <?php
 require_once './../../core/init.php';
 $mode = isset($_POST['mode'])? sanitize($_POST['mode']):'';
-$edit_id = isset($_POST['edit_id '])? sanitize($_POST['edit_id ']):'';
+$edit_id = isset($_POST['edit_id'])? sanitize($_POST['edit_id']):'';
 $cartQ= $db->query("SELECT * FROM cart WHERE id ='{$cart_id}'");
 $result = mysqli_fetch_assoc($cartQ);
 $items = json_decode($result['items'],true);
 $updated_items=array();
 $domain = ($_SERVER['HTTP_HOST'] != 'localhost')?'.'.$_SERVER['HTTP_HOST']:false;
 
+
 if($mode == 'removeone'){
+
   foreach ($items as $item ) {
+    if($item['id'] == $edit_id){
       $item['quantity'] =$item['quantity'] - 1;
       if($item['quantity'] > 0){
-        $updated_items[] = $item;
+
 
           }
 
       }
+      $updated_items[] = $item;
+}
 }
 
 else {
-  foreach ($items as $item ) {
+  foreach ($items as $item  ) {
+    if($item['id'] == $edit_id){
       $item['quantity'] = $item['quantity'] + 1;
-      $updated_items[] = $item;
-  }
 
+  }
+    $updated_items[] = $item;
+    }
 }
 
 if(!empty($updated_items)){
