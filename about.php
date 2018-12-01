16e11453a6013304e4829604abc3c009f8d70d54
Lukarrio1 <40766583+Lukarrio1@users.noreply.github.com>
2018-12-01 11:32:15 -0500

    pdf

--- about.php
+++ about.php
@@ -8,6 +8,7 @@
 	<!-- Main Content -->
 	<div class="col-md-8">
 		<div class="row">
+		<div class="col-4">
 			<h2 class="text-center">About us</h2>
             <img src="./qrcode.php" alt="Social Media links" srcset="">
 			<br>
@@ -15,7 +16,11 @@
 			Scan for social links..
 			</span>
 		</div>
+		<div class="col-12 text-center">
+		<a href="./pdf.php" class="btn btn-defualt">Terms and Conditions</a>
+		</div>
 	</div>
+</div>
 <?php
  include 'includes/rightbar.php';
  include 'includes/footer.php';
---
+++ pdf.php
@@ -0,0 +1,16 @@
+<?php 
+// this is responible for the pdf 
+require("./pdf/fpdf.php");
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont("Arial","B",16);
+    $pdf->Cell(0,20,'Terms & Conditions',10,1,'C');
+    $pdf->Cell(0,10,'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit necessitatibus amet veniam ',10,1,'C');
+    $pdf->Cell(0,10,"aspernatur! Facilis totam ad inventore autem commodi quis natus porro molestiae impedit ",10,1,'C');
+    $pdf->Cell(0,10," obcaecati blanditiis necessitatibus quo fugit eligendi laudantium, repellat, pariatur od",10,1,'C');
+    $pdf->Cell(0,10,"similique dolore tempore",10,1,'C');
+    $pdf->output();
+
+
+
+
